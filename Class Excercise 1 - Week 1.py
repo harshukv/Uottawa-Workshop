bdc6b4437bd18ed3edb1be4ab9ea24ca4a757b8f
bugthecode <104872905+bugthecode@users.noreply.github.com>
2022-05-03 19:38:18 -0400

    Add files via upload

---
+++ Class Excercise 1 - Week 1.py
@@ -0,0 +1,16 @@
+
+
+I1 = int(input('enter first number'))
+I2 = int(input('enter second number'))
+I = I1+I2
+print(I)
+J = I1-I2
+print(J)
+k = I1*I2
+print(k)
+l = I1**I2
+print(l)
+m = I1%I2
+print(m)
+n = I1/I2
+print(n)
