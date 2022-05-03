6956c2310057532b167d635efd730cfb5a87953a
harshukv <69118352+harshukv@users.noreply.github.com>
2022-05-03 19:53:02 -0400

    Update Class Excercise 1 - Week 1.py

--- Class Excercise 1 - Week 1.py
+++ Class Excercise 1 - Week 1.py
@@ -2,15 +2,16 @@
 
 I1 = int(input('enter first number'))
 I2 = int(input('enter second number'))
-I = I1+I2
+I3 = int(input('enter third number'))
+I = I1+I2+I3
 print(I)
-J = I1-I2
+J = I1-I2-I3
 print(J)
-k = I1*I2
+k = I1*I2*I3
 print(k)
-l = I1**I2
+l = I1**I2**I3
 print(l)
-m = I1%I2
+m = I1%I2%I3
 print(m)
-n = I1/I2
+n = I1/I2/I3
 print(n)
